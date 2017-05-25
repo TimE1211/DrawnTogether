137615fc2716b7d4ddf308977165b2cc16d69611
TimE1211 <timmy121192@gmail.com>
2017-05-25 17:29:22 -0400

    setting up api to recieve JSON format according to IOS app and display that data

--- Sources/Project.swift
+++ Sources/Project.swift
@@ -1,8 +1,8 @@
 //
-//  Line.swift
+//  Project.swift
 //  DrawnTogether
 //
-//  Created by Timothy Hang on 5/21/17.
+//  Created by Timothy Hang on 5/25/17.
 //
 //
 
@@ -10,44 +10,43 @@ import StORM
 import SQLiteStORM
 import PerfectLib
 
-class Line: SQLiteStORM
+class Project: SQLiteStORM
 {
-  var id = 0
-  var startx = "0"
-  var starty = "0"
-  var endx = "0"
-  var endy = "0"
+  var projectUUID: String
+  var name: String
+  var users: [User] = []
+  var lines: [Line] = []
   
   override open func table() -> String
   {
-    return "Lines"
+    return "Projects"
   }
-
+  
   override func to(_ this: StORMRow)
   {
-    id = this.data["id"] as? Int ?? 0
+    projectUUID = this.data["projectUUID"] as? Int ?? 0
     startx = this.data["startx"] as! String
     starty = this.data["starty"] as! String
     endx = this.data["endx"] as! String
     endy = this.data["endy"] as! String
   }
   
-  func rows() -> [Line]
+  func rows() -> [Project]
   {
-    var lines = [Line]()
+    var projects = [Project]()
     for i in 0..<self.results.rows.count
     {
-      let line = Line()
-      line.to(self.results.rows[i])
-      lines.append(line)
+      let project = Project()
+      project.to(self.results.rows[i])
+      projects.append(project)
     }
-    return lines
+    return projects
   }
-
+  
   override public func setup()
   {
     do {
-      try sqlExec("CREATE TABLE IF NOT EXISTS Lines (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, startx TEXT, starty TEXT, endx TEXT, endy TEXT)")
+      try sqlExec("CREATE TABLE IF NOT EXISTS Projects (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, startx TEXT, starty TEXT, endx TEXT, endy TEXT)")
       //users INTEGER FOREIGN KEY
     } catch
     {
---
+++ Sources/User.swift
@@ -0,0 +1,60 @@
+//
+//  User.swift
+//  DrawnTogether
+//
+//  Created by Timothy Hang on 5/25/17.
+//
+//
+
+
+import StORM
+import SQLiteStORM
+import PerfectLib
+
+class User: SQLiteStORM
+{
+  var username = ""
+  var password = ""
+
+  override open func table() -> String
+  {
+    return "Users"
+  }
+  
+  override func to(_ this: StORMRow)
+  {
+    username = this.data["username"] as! String
+    password = this.data["password"] as! String
+  }
+  
+  func rows() -> [User]
+  {
+    var users = [User]()
+    for i in 0..<self.results.rows.count
+    {
+      let user = User()
+      user.to(self.results.rows[i])
+      users.append(user)
+    }
+    return users
+  }
+  
+  override public func setup()
+  {
+    do {
+      try sqlExec("CREATE TABLE IF NOT EXISTS Users (username TEXT PRIMARY KEY NOT NULL, password TEXT)")
+    } catch
+    {
+      print(error)
+    }
+  }
+  
+  func asDictionary() -> [String: Any]
+  {
+    return [
+      "username": self.username,
+      "password": self.password
+    ]
+  }
+}
+
